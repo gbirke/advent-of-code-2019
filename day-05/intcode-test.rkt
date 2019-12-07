433241ddb9032503ddc5bb235a7292dd513e316d
Gabriel Birke <gabriel.birke@gmail.com>
2019-12-07 19:29:00 +0100

    Simplify IO logic
    Less functional closures, more parameters

--- day-05/intcode-test.rkt
+++ day-05/intcode-test.rkt
@@ -4,37 +4,8 @@
 		 racket/list
          "intcode.rkt")
 
-(let ([start-io (create-io '(42 23))])
-  (check-equal? (apply (first start-io) '() ) '((42 23) ()) "starts with empty io" )
-  (apply (second start-io) (list (lambda (first-input-io input-value) 
-	  (check-equal? input-value 42 "First input value" )
-	  (check-equal? (apply (first first-input-io) '() ) '((23) ()) "reads io")
-	  (let ([first-output-io (apply (third first-input-io) (list (lambda () 4711)))])
-		  (check-equal? (apply (first first-output-io) '() ) '((23) (4711)) "appends to output" )
-		  (let ([second-output-io (apply (third first-output-io) (list (lambda () 128)))])
-			  (check-equal? (apply (first second-output-io) '() ) '((23) (128 4711)) "appends to output" )
-		  )
-	  )
-	  (apply (second first-input-io) (list (lambda (second-input-io input-value)
-    	  (check-equal? input-value 23 "Second input value" )
-		  (check-equal? (apply (first second-input-io) '() ) '(() ()) "reads io" )
-	  )))
-  )))
-  ; Check without apply
-  (io-get-input start-io (lambda (first-input-io input-value)
-	(check-equal? input-value 42 "First input value")
-	(check-equal? (io-get-state first-input-io) '((23) ()))
-  ))
-)
 
-(let ([opcode (op-input '(3 2 0) 0 42)])
-	(check-equal? (run-op opcode) '(3 2 42) "Input writes signified address")
-)
-
-(let ([opcode (op-output '(4 2 0) 0)])
-	(check-equal? (run-op opcode) '(4 2 0) "output leaves memory as-is")
-)
-(check-equal? (run-program '(3 0 3 1 99) (create-io '(42 23))) '(() () (42 23 3 1 99)) "Input writes input at address")
-(check-equal? (run-program '(4 0 4 4 99) (create-io '())) '((99 4) () (4 0 4 4 99)) "Output reads from address")
+(check-equal? (run-program '(3 0 3 1 99) '(42 23)) '(() () (42 23 3 1 99)) "Input writes input at address")
+(check-equal? (run-program '(4 0 4 4 99)) '((99 4) () (4 0 4 4 99)) "Output reads from address")
 
 
--- day-05/intcode.rkt
+++ day-05/intcode.rkt
@@ -30,79 +30,52 @@
 	(read-memory memory (+ address 3))
 ))
 
-(define (op-input memory address value)
-  (list
-	(lambda (write-address) (write-to-memory memory write-address value))
-	(read-memory memory (+ address 1))
-))
-
-(define (op-output memory address)
-  (list
-	(lambda (output-address) memory)
-	(read-memory memory (+ address 1))
-))
-
 (define (run-op opcode)
   (apply (first opcode) (rest opcode )))
 
-(define (run-program memory io [instruction-pointer 0])
-  (define (run-instruction opcode new-io)
+(define (run-program memory [input '()] [output '()] [instruction-pointer 0])
+  (define (run-instruction opcode)
 	(run-program 
 	  (run-op opcode)
-	  new-io
+	  input
+	  output
 	  (+ instruction-pointer (length opcode) )
+	  ))
+  (define (run-input)
+	(run-program
+	  (write-to-memory
+		memory 
+		(read-memory memory (+ instruction-pointer 1))
+		(first input)
+		)
+	  (rest input)
+	  output
+	  (+ instruction-pointer 2 )
+	))
+  (define (run-output)
+  	(run-program
+	  memory
+	  input
+	  (cons (read-memory-pointer memory (+ instruction-pointer 1)) output)
+	  (+ instruction-pointer 2 )
 	  ))
   (let ([opcode (list-ref memory instruction-pointer)])
 	(cond
-			[(= opcode 99) (let ([state (io-get-state io )]) (append (reverse state) (list memory)))]
-			[(= opcode 1) (run-instruction (op-addition memory instruction-pointer io))]
-			[(= opcode 2) (run-instruction (op-multiplication memory instruction-pointer io))]
-			[(= opcode 3) (io-get-input io (lambda (new-io input-value)
-							(run-instruction 
-							  (op-input memory instruction-pointer input-value)
-							  new-io))) ]
-			[(= opcode 4) (let ([op (op-output memory instruction-pointer)])
-							 (run-instruction 
-							   op 
-							   (io-write-output io (lambda () 
-													 (read-memory memory (second op)) )
-								))
-							 )]
+			[(= opcode 99) (list output input memory)]
+			[(= opcode 1) (run-instruction (op-addition memory instruction-pointer))]
+			[(= opcode 2) (run-instruction (op-multiplication memory instruction-pointer))]
+			[(= opcode 3) (run-input)]
+			[(= opcode 4) (run-output)]
 	     ))
 	
 	)
-
-; Wrap input and output state in callbacks
-(define (create-io [current-input '()] [current-output '()] )
-  (define (read-input fn)
-	(fn (create-io (rest current-input) current-output) (first current-input))
-  )
-  (define (write-output fn)
-	(create-io current-input (cons (apply fn '()) current-output))
-  )
-  (define (current) (list current-input current-output))
-  (list current read-input write-output)
-)
-
-(define (io-get-state io) (apply (first io) '()))
-
-(define (io-get-input io fn) 
-	(apply (second io) (list fn)))
-
-(define (io-write-output io fn) 
-	(apply (third io) (list fn)))
 
 (define (read-program str)
   (map string->number (map string-trim (string-split str ","))))
 
 (provide op-addition
 		 op-multiplication
-		 op-input
-		 op-output
 		 run-op
 		 run-program
 		 read-program
-		 create-io
-		 io-get-input
-		 io-get-state
 		 )
