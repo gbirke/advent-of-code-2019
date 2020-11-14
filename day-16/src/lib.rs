b3b9a4e1e4796f6b1fd23b1b728a24852345c8cc
Gabriel Birke <gabriel.birke@gmail.com>
2020-11-14 13:16:31 +0100

    Add pattern multiplication
    Use iterator and zip to multiply

--- day-16/src/lib.rs
+++ day-16/src/lib.rs
@@ -27,28 +27,63 @@ fn pattern(output_position: u32) -> Pattern {
     }
 }
 
+fn multiply_with_pattern(digits: Vec<i8>, output_position: u32) -> Vec<i8> {
+    digits
+        .iter()
+        .zip(pattern(output_position))
+        .map(|(digit, pattern_multiplier)| digit * pattern_multiplier)
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // Pattern
+
     #[test]
-    fn pattern_first_digit() {
+    fn pattern_repeats_once_on_first_digit() {
         let result: Vec<i8> = pattern(1).take(7).collect();
 
         assert_eq!(vec![1, 0, -1, 0, 1, 0, -1], result);
     }
 
     #[test]
-    fn pattern_second_digit() {
+    fn pattern_repeats_twice_on_second_digit() {
         let result: Vec<i8> = pattern(2).take(15).collect();
 
-        assert_eq!(vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1, -1], result);
+        assert_eq!(
+            vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1, -1],
+            result
+        );
     }
 
     #[test]
-    fn pattern_third_digit() {
+    fn pattern_repeats_three_times_on_third_digit() {
         let result: Vec<i8> = pattern(3).take(20).collect();
 
-        assert_eq!(vec![0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1, 0, 0, 0, 1, 1, 1, 0, 0, 0], result);
+        assert_eq!(
+            vec![0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            result
+        );
     }
+
+    // Multiplication
+
+    #[test]
+    fn multiply_with_pattern_first_digit() {
+        let input = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        let result = multiply_with_pattern(input, 1);
+
+        assert_eq!(vec![1, 0, -3, 0, 5, 0, -7, 0, 9, 0], result)
+    }
+
+    #[test]
+    fn multiply_with_pattern_second_digit() {
+        let input = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        let result = multiply_with_pattern(input, 2);
+
+        assert_eq!(vec![0, 2, 3, 0, 0, -6, -7, 0, 0, 0], result)
+    }
+
 }
