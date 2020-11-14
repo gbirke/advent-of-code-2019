f0b66ac7476548f652d71e113110014f098f2c8f
Gabriel Birke <gabriel.birke@gmail.com>
2020-11-14 20:56:16 +0100

    Implement Part 1 of the solution
    Read input file and print output

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,6 +1,15 @@
 use fft::fft;
+use std::fs;
+use std::str;
+
+fn read_input() -> Vec<u8> {
+    let content = fs::read_to_string("input.txt").expect("input.txt not found");
+    content.trim().as_bytes().iter().map(|b| b - 48 ).collect()
+}
 
 fn main() {
-    let result = fft(&[1, 2, 3], 1);
-    println!("{:?}", result);
+    let mut result: Vec<u8> = fft(&read_input(), 100);
+    result.truncate(8);
+    let bytes: Vec<u8> = result.iter().map(|b| b+48).collect();
+    println!("{:?}", str::from_utf8(&bytes).expect("could not convert into utf8"))
 }
